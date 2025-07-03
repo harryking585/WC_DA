8a534173a00dfabe75e36db9d47549f8c122bd1f
Harrison King <harryking585@gmail.com>
2025-07-03 10:50:04 -0500

    Formatting Fixed

--- react/index.js
+++ react/index.js
@@ -1,17 +1,17 @@
 function MyButton() {
-    return (
-      <button>
-        Generate Report
-      </button>
-    );
-  }
+  return (
+    <button>
+      Generate Report
+    </button>
+  );
+}
   
-  export default function MyApp() {
-    return (
-      <div>
-        <h1>WC_DA</h1>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>WC_DA</h1>
         <MyButton />
       </div>
-    );
-  }
+  );
+}
   
